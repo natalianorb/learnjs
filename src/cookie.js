237364e5066915720efc05b99ef183ad878133c1
Natalya Norboyeva <divonata@gmail.com>
2017-02-24 23:10:48 +0900

    hw7 second commit

--- src/cookie.js
+++ src/cookie.js
@@ -38,6 +38,19 @@ let addNameInput = homeworkContainer.querySelector('#add-name-input');
 let addValueInput = homeworkContainer.querySelector('#add-value-input');
 let addButton = homeworkContainer.querySelector('#add-button');
 let listTable = homeworkContainer.querySelector('#list-table tbody');
+const createCookie = require('./index').createCookie;
+const deleteCookie = require('./index').deleteCookie;
+function getCookies() {
+    return document.cookie
+        .split('; ')
+        .filter(Boolean)
+        .map(cookie => cookie.match(/^([^=]+)=(.+)/))
+        .reduce((obj, [, name, value]) => {
+            obj[name] = value;
+
+            return obj;
+        }, {});
+}
 
 /**
  * Функция должна проверять встречается ли подстрока chunk в строке full
@@ -53,6 +66,10 @@ let listTable = homeworkContainer.querySelector('#list-table tbody');
  * @return {boolean}
  */
 function isMatching(full, chunk) {
+    full = full.toLowerCase();
+    chunk = chunk.toLowerCase();
+
+    return full.includes(chunk);
 }
 
 /**
@@ -62,11 +79,91 @@ function isMatching(full, chunk) {
  * @param value - значение cookie
  */
 function createCookieTr(name, value) {
+    let tr = document.createElement('tr');
+    let nameTd = document.createElement('td');
+    let tdValue = document.createElement('td');
+    let tdButton = document.createElement('td');
+    let removeButton  = document.createElement('button');
+
+
+    nameTd.textContent = name;
+    tdValue.textContent = value;
+    removeButton.textContent = 'Удалить';
+    removeButton.addEventListener('click', function (event) {
+        let tr = event.target.closest('tr');
+        let nameTd = tr.firstElementChild;
+
+        deleteCookieTr(nameTd);
+    });
+    tdButton.appendChild(removeButton);
+
+    tr.appendChild(nameTd);
+    tr.appendChild(tdValue);
+    tr.appendChild(tdButton);
+
+    listTable.appendChild(tr);
 }
 
-filterNameInput.addEventListener('keyup', function() {
-});
+function updateCookieTr(name, value) {
+    let nameTd = findNametd(name);
+
+    if (nameTd) {
+        nameTd.nextElementSibling.textContent = value;
+    } else {
+        createCookieTr(name, value);
+    }
+}
+
+function deleteCookieTr(nameTd) {
+    let tr = nameTd.closest('tr');
+
+    deleteCookie(nameTd.innerText);
+    listTable.removeChild(tr);
+}
+
+function findNametd(name) {
+    for (let tr of listTable.children) {
+        if (tr.firstElementChild.innerText == name) {
+            return tr.firstElementChild;
+        }
+    }
+
+    return null;
+}
+
+function filter () {
+    let value = filterNameInput.value.trim();
+    let cookies = getCookies();
+
+    listTable.innerHTML = null;
+    if (value != '') {
+        for (let key of Object.keys(cookies)) {
+            if (isMatching(key, value) || isMatching(cookies[key], value)) {
+                createCookieTr(key, cookies[key]);
+            }
+        }
+    } else {
+        for (let key of Object.keys(cookies)) {
+            createCookieTr(key, cookies[key]);
+        }
+    }
+}
+
+filterNameInput.addEventListener('keyup', filter);
 
 addButton.addEventListener('click', () => {
+    let name = addNameInput.value.trim();
+    let value = addValueInput.value.trim();
+    let cookies = getCookies();
+
+    createCookie(name, value);
+    if (name in cookies) {
+        updateCookieTr(name, value);
+    } else {
+        createCookieTr(name, value);
+    }
+    filter();
+
 });
+
 
--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@
  * @return {Window}
  */
 function createWindow(name, width, height) {
+    return open('', name, 'width='+width+',height='+height);
 }
 
 /**
@@ -17,6 +18,7 @@ function createWindow(name, width, height) {
  * @param {Window} window - окно, размер которого надо изменить
  */
 function closeWindow(window) {
+    window.close();
 }
 
 /**
@@ -26,6 +28,7 @@ function closeWindow(window) {
  * @param value - значение
  */
 function createCookie(name, value) {
+    document.cookie = '' + name + '=' + value + '; ';
 }
 
 /**
@@ -34,6 +37,8 @@ function createCookie(name, value) {
  * @param name - имя
  */
 function deleteCookie(name) {
+    let exp = new Date(0);
+    document.cookie=''+ name+'=;expires='+exp;
 }
 
 export {
